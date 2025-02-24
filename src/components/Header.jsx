3584f3ae27d1154c7bab462ba811f351c31d5d84
amankumar72 <amankumar721088@gmail.com>
2025-02-24 22:03:55 +0530

    fix some responsiveness related issues

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const navigate = useNavigate();
 
-  const [isActive, setIsActive] = useState("home")
+  const [isActive, setIsActive] = useState("home");
 
   return (
     <nav className="p-4 w-full shadow-md shadow-zinc-500 border-b mb-3 border-zinc-600 ">
@@ -22,32 +23,60 @@ const Header = () => {
         </button>
 
         {/* Navigation Links */}
-        <ul
+        <div
           className={`md:flex md:items-center md:space-x-6 lg:text-xl absolute md:static z-10 bg-[#202020] md:bg-transparent w-full left-0 md:w-auto transition-all duration-300 ease-in ${
             isOpen ? "top-16" : "top-[-380px]"
           } md:top-0`}
         >
-          <li className={`p-4 md:px- md:py-1 border-b border-white md:border-none ${isActive=='home' && 'text-blue-500'} hover:bg-zinc-700 rounded`}>
-            <Link to="/" onClick={() => {setIsActive("home");setIsOpen(false)}} >
-              Home
-            </Link>
-          </li>
-          <li className={`p-4 md:px- md:py-1 border-b border-white md:border-none ${isActive=='about' && 'text-blue-500'} hover:bg-zinc-700 rounded`}>
-            <Link to="/about" onClick={() =>{setIsActive("about");setIsOpen(false)}} >
-              About
-            </Link>
-          </li>
-          <li className={`p-4 md:px- md:py-1 border-b border-white md:border-none ${isActive=='country' && 'text-blue-500'} hover:bg-zinc-700 rounded`}>
-            <Link to="/country" onClick={() =>{setIsActive("country");setIsOpen(false)}} >
-              country
-            </Link>
-          </li>
-          <li className={`p-4 md:px- md:py-1 border-b border-white md:border-none ${isActive=='contact' && 'text-blue-500'} hover:bg-zinc-700 rounded`}>
-            <Link to="/contact"  onClick={() =>{setIsActive("contact");setIsOpen(false)}} >
-              Contact
-            </Link>
-          </li>
-        </ul>
+          <div
+            className={`p-4 md:px- md:py-1 border-b border-white md:border-none ${
+              isActive == "home" && "text-blue-500"
+            } hover:bg-zinc-700 rounded`}
+            onClick={() => {
+              setIsActive("home");
+              setIsOpen(false);
+              navigate("/");
+            }}
+          >
+            Home
+          </div>
+          <div
+            className={`p-4 md:px- md:py-1 border-b border-white md:border-none ${
+              isActive == "about" && "text-blue-500"
+            } hover:bg-zinc-700 rounded`}
+            onClick={() => {
+              setIsActive("about");
+              setIsOpen(false);
+              navigate("/about");
+            }}
+          >
+            About
+          </div>
+          <div
+            className={`p-4 md:px- md:py-1 border-b border-white md:border-none ${
+              isActive == "country" && "text-blue-500"
+            } hover:bg-zinc-700 rounded`}
+            onClick={() => {
+              setIsActive("country");
+              setIsOpen(false);
+              navigate("/country");
+            }}
+          >
+            country
+          </div>
+          <div
+            className={`p-4 md:px- md:py-1 border-b border-white md:border-none ${
+              isActive == "contact" && "text-blue-500"
+            } hover:bg-zinc-700 rounded`}
+            onClick={() => {
+              setIsActive("contact");
+              setIsOpen(false);
+              navigate("/contact");
+            }}
+          >
+            contact
+          </div>
+        </div>
       </div>
     </nav>
   );
